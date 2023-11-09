5de26904f621a7f16c856341eb8e919fecbd9211
matheusPierro <matpierro570@gmail.com>
2023-11-09 01:50:43 -0300

    feat: new project created with spring initializr and correct dependencies in maven and delete past project that use just jpa

---
+++ src/main/java/br/com/system/syncfy/SyncfyApplication.java
@@ -0,0 +1,13 @@
+package br.com.system.syncfy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncfyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyncfyApplication.class, args);
+	}
+
+}
