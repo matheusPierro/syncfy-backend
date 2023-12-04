2b9fcbc66c2dc6bf2a69eeae4eb50835aad330ea
Thiago Castro <thiagogyorgy.castro@gmail.com>
2023-12-04 20:05:21 -0300

    deletando repository da pedidoproduto

--- src/main/java/br/com/system/syncfy/model/repository/PedidoProdutoRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.system.syncfy.model.repository;
-
-import br.com.system.syncfy.model.entity.PedidoProduto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PedidoProdutoRepository extends JpaRepository<PedidoProduto, Long> {
-}
