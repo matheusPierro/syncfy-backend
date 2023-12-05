462432a89d960b0bc8e6d28fcfa8feefa17f29e2
Thiago Castro <thiagogyorgy.castro@gmail.com>
2023-12-04 22:11:28 -0300

    criando dtos

--- src/main/java/br/com/system/syncfy/model/dto/BairroDTO.java
+++ src/main/java/br/com/system/syncfy/model/dto/BairroDTO.java
@@ -1,9 +1,11 @@
 package br.com.system.syncfy.model.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
-public record DadosBairro(
+public record BairroDTO(
         @NotBlank(message = "O bairro não pode estar em branco.")
+        @Pattern(regexp = "[a-zA-Z]+")
         String bairro
 ) {
 }
--- src/main/java/br/com/system/syncfy/model/dto/DadosPais.java
+++
@@ -1,9 +0,0 @@
-package br.com.system.syncfy.model.dto;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record DadosPais(
-        @NotBlank(message = "O pais não pode estar em branco.")
-        String pais
-) {
-}
